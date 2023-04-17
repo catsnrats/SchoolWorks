d47f68a0beb9c847c8d71509faeec2acc9abce5f
Antti Seppänen <ac8445@student.jamk.fi>
2023-04-17 17:59:04 +0300

    Tehtava Pilli alkuun olioiden luonnilla...

---
+++ PilliTehtava/Pilli.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PilliTehtava
+{
+    internal class Pilli
+    {
+        public string aani;
+
+        public Pilli(String pillinAani) 
+        {
+            aani = pillinAani;
+        }
+
+        public void soi() 
+        { }
+    }
+}
---
+++ PilliTehtava/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PilliTehtava
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Luokasta pilli johdetut oliot
+            Pilli sorsapilli = new Pilli("Kwaak");
+            Pilli kukkopilli = new Pilli("Peef");
+
+            Console.WriteLine(sorsapilli.aani); // Kokeiltu oliota
+
+            Console.Read();
+        }
+    }
+}
