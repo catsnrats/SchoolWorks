c74e6de90f84c1c4ff672d06ae56d19f4b54cb9e
Antti Seppänen <ac8445@student.jamk.fi>
2023-04-18 16:57:22 +0300

    Musiikki-tehtävä alkuun...

---
+++ Musiikki/Musiikkikappale.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Musiikki
+{
+    internal class Musiikkikappale
+    {
+        private string nimi;
+        private int pituus;
+
+        public Musiikkikappale(string nimi, int pituus)
+        {
+            this.nimi = nimi;
+            this.pituus = pituus;
+        }
+    }
+}
