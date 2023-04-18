a49f55f1afde9ff9bc5281b228bb6524ab18c139
Antti Seppänen <ac8445@student.jamk.fi>
2023-04-18 10:36:29 +0300

    Tuote tehtävä repoon

---
+++ Tuote/Tuote.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tuote
+{
+    internal class Tuote
+    {
+        // Luokan muuttujat merkkijono, desimaali- ja kokonaisluku
+        public string nimi;
+        public double hinta;
+        public int maara;
+
+        // Parametrillinen konstruktori Tuote
+        public Tuote(String nimiAlussa, double hintaAlussa, int maaraAlussa)
+        {
+            nimi = nimiAlussa;
+            hinta = hintaAlussa;
+            maara = maaraAlussa;
+        }
+
+        public void TulostaTuote() // Metodi olion tietojen tulostukseen
+        {
+            Console.WriteLine(nimi + ":" + " Hinta: " + hinta + " eur, " + maara + " kpl");
+        } 
+    }
+}
