ba4243494282852d6f2371f9ac0fe69c5c38c332
Antti Seppänen <ac8445@student.jamk.fi>
2023-04-18 11:37:37 +0300

    Velka-tehtävälle runko

---
+++ Velka/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Velka
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Velka laina = new Velka(1000, 5.2); // Velka-luokasta johdettu olio laina
+        }
+    }
+}
---
+++ Velka/Velka.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Velka
+{
+    internal class Velka
+    {
+        // Desimaalimuuttujat luokkaan
+        public double saldo;
+        public double korkoProsentti;
+
+        public Velka(double saldo, double korkoProsentti) // Parametrillinen konstruktori
+        {
+            this.saldo = saldo; // Käytetty this.-muotoa koska muuttujien nimet pysyvät samoina
+            this.korkoProsentti = korkoProsentti;
+        }
+
+        public void TulostaSaldo()
+        {
+            saldo = 0;
+        }
+        public void OdotaVuosi()
+        { }
+    }
+}
