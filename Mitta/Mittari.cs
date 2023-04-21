a17eaa3c02a30a822023ef23f4a35d9643111a82
Antti <antti.se@gmail.com>
2023-04-21 15:17:42 +0300

    Mitta aluille

---
+++ Mitta/Mittari.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mitta
+{
+    internal class Mittari
+    {
+        private int _mitta; // Private kenttä luokkaan Mittari
+
+        public Mitta() // Paremetritön konstruktori. Asettaa _mitta kentän arvoksi 0
+        { 
+            _mitta = 0;
+        }
+    }
+}
