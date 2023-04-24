e01be55fc9500acd7de1fa20adbdc3e8dc3effc4
Antti Seppänen <ac8445@student.jamk.fi>
2023-04-24 11:15:58 +0300

    metodeja ja while-loopin testaus

--- Mitta/Mittari.cs
+++ Mitta/Mittari.cs
@@ -10,9 +10,24 @@ namespace Mitta
     {
         private int _mitta; // Private kenttä luokkaan Mittari
 
-        public Mittari() // Paremetritön konstruktori. Asettaa _mitta kentän arvoksi 0
+        public Mittari() // Paremetritön konstruktori. Asettaa _mitta-kentän arvoksi 0
         { 
             _mitta = 0;
+        }
+        public void Lisaa()
+        {
+            _mitta += 1;
+        }
+        public void Vahenna()
+        {
+            _mitta -= 1;
+        }
+        public int Mitta()
+        { return _mitta; }
+        public Boolean Taynna()
+        {
+            if (_mitta >= 5);
+                return false; 
         }
     }
 }
--- Mitta/Program.cs
+++ Mitta/Program.cs
@@ -11,6 +11,23 @@ namespace Mitta
         static void Main(string[] args)
         {
             Mittari mittari = new Mittari();
+
+            while (!mittari.Taynna()) 
+            {
+                Console.WriteLine("FUBAR");
+            }
+
+            mittari.Lisaa();
+            mittari.Lisaa();
+            mittari.Lisaa();
+            mittari.Lisaa();
+            mittari.Lisaa();
+            mittari.Lisaa();
+            /* mittari.Vahenna();
+            mittari.Vahenna();
+            mittari.Vahenna(); */
+            Console.WriteLine(mittari.Mitta());
+            Console.Read();
         }
     }
 }
