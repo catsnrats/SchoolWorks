09a21f5bb066302c6d474ba66113fadb2a625fd4
Antti Seppänen <antti.se@gmail.com>
2023-04-24 18:14:48 +0300

    LataaRahaa()-metodi

---
+++ Maksukortti/Maksukortti.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maksukortti
+{
+    internal class Maksukortti
+    {
+        private double _saldo;
+
+        public Maksukortti(double saldo)
+        {
+            _saldo = saldo;
+        }
+        public double LataaRahaa() 
+        {
+            Console.WriteLine("Kortille ladattava summa: ");
+            _saldo += int.Parse(Console.ReadLine());
+            return _saldo;
+        }
+
+        /*public override string ToString()
+        {
+            return base.ToString();
+        }*/
+    }
+}
---
+++ Maksukortti/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maksukortti
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Maksukortti kortti = new Maksukortti(50);
+
+           
+            Console.WriteLine($"Kortin saldo: {kortti.LataaRahaa()}");
+
+            Console.ReadLine();
+        }
+    }
+}
