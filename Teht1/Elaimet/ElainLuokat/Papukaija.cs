ba5e782e24a79f33504774b5beb0a16ceec21ada
Antti <antti.se@gmail.com>
2023-05-09 19:20:30 +0300

    lisätty muutama luokka

---
+++ Teht1/Elaimet/ElainLuokat/Papukaija.cs
@@ -0,0 +1,13 @@
+﻿using ElainLuokat;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elaimet
+{
+    internal class Papukaija : Linnut
+    {
+    }
+}
