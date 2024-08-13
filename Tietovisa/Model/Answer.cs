7d352069ec55c004b875fe78b988c52cad82b5b2
Antti <antti.se@gmail.com>
2024-08-13 04:15:08 +0300

    answer class

---
+++ Tietovisa/Model/Answer.cs
@@ -0,0 +1,26 @@
+﻿namespace Tietovisa.Model
+{
+    public class Answer
+    {
+        public int Id {  get; set; }
+
+        public int QuestionId { get; set; }
+
+        public string AnswerText { get; set; }
+
+        public string TopicInfo { get; set; }
+
+        public bool IsCorrect { get; set; }
+
+        public Answer() { }
+
+        public Answer(int id, int questionId, string answerText, string topicInfo, bool isCorrect)
+        {
+            Id = id;
+            QuestionId = questionId;
+            AnswerText = answerText;
+            TopicInfo = topicInfo;
+            IsCorrect = isCorrect;
+        }        
+    }
+}
